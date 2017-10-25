ee975d936946913485a1c3ff412d0355e13a0f89
Toptachamann <timofey.chudakov@gmail.com>
2017-10-25 19:39:55 +0300

    Bug fixed

--- src/graphics/LPSolverFrame.java
+++ src/graphics/LPSolverFrame.java
@@ -2,13 +2,17 @@ package graphics;
 
 import graphics.utils.GBC;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -40,7 +44,18 @@ public class LPSolverFrame extends JFrame {
         JScrollPane outputScrollPane = new JScrollPane(outputArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, inputScrollPane, outputScrollPane);
         splitPane.setResizeWeight(0.5);
-        mainPanel.add(splitPane, new GBC(0, 0, 1, 1, 1, 1));
+
+
+        JPanel titlePanel = new JPanel(new GridBagLayout());
+        titlePanel.setBackground(new Color(40, 40, 40));
+        JLabel lpLabel = new JLabel("LPSolver");
+        Font lpLabelFont = new Font("Sans Serif", Font.BOLD, 16);
+        lpLabel.setFont(lpLabelFont);
+        lpLabel.setForeground(Color.white);
+        titlePanel.add(lpLabel, new GBC(0, 0, 1, 1, 0, 0).setAnchor(GBC.CENTER).setInsets(10, 0, 10, 0));
+
+        mainPanel.add(titlePanel, new GBC(0, 0, 1, 1, 1, 0).setAnchor(GBC.CENTER));
+        mainPanel.add(splitPane, new GBC(0, 1, 1, 1, 1, 1));
         this.getContentPane().add(mainPanel);
     }
 }
